3fd30497e52da91327fdcbc4f02494a554616ea8
apetrogianni <apetrogianni@uth.gr>
2022-07-21 11:35:37 +0300

    minor

--- data_parsing/feature_extraction.py
+++ data_parsing/feature_extraction.py
@@ -6,6 +6,7 @@ import numpy as np
 from multimodal_movie_analysis.analyze_visual.analyze_visual \
     import dir_process_video, process_video
 
+
 def feature_extraction(videos_path):
     """
     Extracts features from videos and saves them
