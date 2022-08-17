cf705b00131e3b971e3ff2ccf21499fc54b2bace
apetrogianni <apetrogianni@uth.gr>
2022-08-18 01:07:49 +0300

    PCA

--- utils/data_scaling.py
+++ utils/data_scaling.py
@@ -8,6 +8,53 @@ import matplotlib
 matplotlib.use('Agg')
 
 
+def zip_time(X):
+    X_extended = []
+    lengths = []
+    for x in X:
+        lengths.append(x.shape[0])
+        for row in x.cpu().detach().numpy():
+            X_extended.append(row)
+    return X_extended, lengths
+
+
+def reconstruct_time_dataset(X, lengths):
+    X_reconstructed = []
+    X_rest = X
+    for length in lengths:
+        X_current = X_rest[:length, :]
+        X_rest = X_rest[length:, :]
+        X_current = torch.Tensor(X_current)
+        X_reconstructed.append(X_current)
+
+    return X_reconstructed
+
+
+class TimeSeriesPCA():
+    def __init__(self, n_components):
+        self.pca = PCA(n_components=n_components)
+
+    def fit(self, X):
+        self.X = X
+        X_extended, _ = zip_time(X)
+        self.pca.fit(X_extended)
+        print("variance: ", self.pca.explained_variance_ratio_.sum())
+        print("n_componenets: ", self.pca.components_.shape[0])
+
+    def transform(self, X):
+        X_extended, lengths = zip_time(X)
+        X_extended_dec = self.pca.transform(X_extended)
+
+        X_dec = reconstruct_time_dataset(X_extended_dec, lengths)
+        return X_dec
+
+    def fit_transform(self, X):
+        self.fit(X)
+        X_dec = self.transform(self.X)
+
+        return X_dec
+
+
 class TimeSeriesStandardScaling():
     def __init__(self):
         pass
